1f78dc6a8e4a380facde075e72cdd7c8289ccd1a
Sakshi Pagariya <96334868+sakshipagaria@users.noreply.github.com>
2022-02-27 15:44:14 +0530

    Add files via upload

---
+++ carani.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    //variables
+    $surface=$('.surface');
+    $car=$('.car');
+    $img=$('.car img');
+    let flag=true;
+
+    const cars=['../ADMIN/OneDrive/Pictures/assets/Img_05.png','../ADMIN/OneDrive/Pictures/assets/Img_06.png'];
+
+    //keypress event
+    $(document).on('keypress',function(e){
+        if(e.which==13){
+            $($surface).toggleClass('right');
+            $($car).toggleClass('suspension');
+        }
+    })
+    //lights
+    $(document).on('keypress',function(e){
+        if(e.which==119){
+            if(flag){
+               flag=false;
+               $img.attr('src',cars[0]);
+           }
+            else{
+               flag=true;
+               $img.attr('src',cars[1]);
+           }
+        }
+    })
+
+});
